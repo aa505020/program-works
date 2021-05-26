f4249cc673f37838fc0f7162dcef350d7f6059a6
unknown <aa505020@hotmail.com>
2021-05-26 20:56:17 +0300

    search and insert by proc

---
+++ form_log_in.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WinFormsApp1
+{
+    public partial class form_log_in : Form
+    {
+        public form_log_in()
+        {
+            InitializeComponent();
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        string con = ("Data Source = DESKTOP-CG69F85\\SQLEXPRESS02; initial catalog = log_in ; integrated security=true");
+        SqlCommand cmd;
+
+        public void check(string a,string b )
+        {
+            SqlConnection sqlcon = new SqlConnection(con);
+
+            cmd = new SqlCommand("searchByUserName", sqlcon);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@userName", a);
+            cmd.Parameters.AddWithValue("@password",b);
+            sqlcon.Open();
+            SqlDataAdapter sqla = new SqlDataAdapter(cmd);
+            DataSet fd = new DataSet();
+            sqla.Fill(fd);
+            sqlcon.Close();
+
+            int count = fd.Tables[0].Rows.Count;
+            if( count == 1 )
+            {
+                MessageBox.Show("HI" + a);
+            }
+            else
+            {
+                MessageBox.Show("errorr");
+            }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            check(textBox1.Text, textBox2.Text);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            form_log_up a = new form_log_up();
+            a.Show();
+
+        }
+
+        private void form_log_in_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ form_log_up.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WinFormsApp1
+{
+    public partial class form_log_up : Form
+    {
+        string con = ("Data Source = DESKTOP-CG69F85\\SQLEXPRESS02; initial catalog = log_in ; integrated security=true");
+        SqlCommand cmd;
+        //public SqlCommand cmd;
+
+      
+        public form_log_up()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            InsertProcInsert(textBox1.Text, textBox2.Text, textBox3.Text);
+
+
+
+
+        }
+        public void InsertProcInsert(string a, string b, string c)
+        {
+            SqlConnection sqlcon = new SqlConnection(con);
+
+            cmd = new SqlCommand("insertTable", sqlcon);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@userName", SqlDbType.VarChar, 50).Value = a;
+            cmd.Parameters.Add("@password", SqlDbType.VarChar, 50).Value = b;
+            cmd.Parameters.Add("@Email", SqlDbType.VarChar, 50).Value = c;
+            sqlcon.Open();
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("HI");
+            sqlcon.Close();
+
+        }
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
